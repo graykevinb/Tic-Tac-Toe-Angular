ce3a26ecc6140de7d466b53288e316cfeab74b20
Kevin Gray <kevin@kevinbgray.com>
2023-04-06 01:14:53 -0500

    Make whole cell clickable

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+
+export class BoardComponent {
+  @Input() width: number[] = this.range(3);
+  @Input() height: number[] = this.range(3);
+
+  private range(max: number): number[] {
+    let range: number[] = [];
+    for (let i = 0; i < max; i++) {
+      range.push(i);
+    }
+    return range;
+  }
+}
---
+++ src/app/cell/cell.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cell',
+  templateUrl: './cell.component.html',
+  styleUrls: ['./cell.component.css']
+})
+export class CellComponent {
+  @Input() pos: string = '';
+
+  move() {
+    console.log(this.pos);
+  }
+}
